c73b3fe0ccef605fb13e5ea974ef19fefe9a2a1c
nlaleman <dragonmyace@gmail.com>
2021-04-19 20:09:32 -0500

    Index

---
+++ src/exercises/Index.java
@@ -0,0 +1,23 @@
+package exercises;
+import java.util.Scanner;
+public class Index {
+    public static void main(String[] args) {
+
+        String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having " +
+                "nothing to do: once or twice she had peeped into the book her sister was reading, but it had no " +
+                "pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures " +
+                "or conversation?'";
+        String aliceLC = alice.toLowerCase();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a search term: ");
+        String search = input.next();
+        String searchLC = search.toLowerCase();
+
+//        int index = aliceLC.indexOf(searchLC);
+//        int length = searchLC.length();
+        String[] parts = aliceLC.split(searchLC);
+                System.out.println(parts[0] + parts[1]);
+
+    }
+}
